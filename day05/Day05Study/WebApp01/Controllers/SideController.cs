0bb1092a6ef723e9c86895be3488bc13d06cb479
hyeonnnho <tiger1330@naver.com>
2025-05-28 17:11:32 +0900

    5일차 커밋

---
+++ day05/Day05Study/WebApp01/Controllers/SideController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+
+namespace WebApp01.Controllers
+{
+    public class SideController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
---
+++ day05/Day05Study/WebApp01/Models/Board.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApp01.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        // Constructor to initialize the board with default values
+        public Board()
+        {
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
